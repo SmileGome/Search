37c4051cec0b9bad7ec8152dfad317db29bb5067
jiwoo <ann9902@gmail.com>
2022-09-27 14:33:47 +0900

    [feat] suffeled dataset split

--- dpr_code/dataset.py
+++ dpr_code/dataset.py
@@ -1,3 +1,4 @@
+from dataclasses import replace
 import json
 from math import comb
 import re
@@ -16,20 +17,19 @@ def split_train_test(file_name):
     with open(file_name, 'r') as origin:
         data = json.load(origin)
 
-    doc_length = len(data['latex_anno'])
+    doc_length = len(data)
     train_len = int(doc_length*0.8)
     left_length = doc_length-train_len
 
     if left_length % 2 == 0:
-        val_len, test_len = left_length//2, left_length//2
+        val_len = left_length//2
     else:
-        val_len, test_len = left_length//2, left_length-(left_length//2)
+        val_len = left_length//2
 
-    # all_doc = np.array(data['latex_anno'])
-    # idx1, idx2, idx3 = np.random.randint(, size=train_len)
-    train_data = data['latex_anno'][:train_len]
-    val_data = data['latex_anno'][train_len:train_len+val_len]
-    test_data = data['latex_anno'][train_len+val_len:]
+    random.shuffle(data)
+    train_data = data[:train_len]
+    val_data = data[train_len:train_len+val_len]
+    test_data = data[train_len+val_len:]
 
     for d in ['train_data', 'val_data', 'test_data']:
         with open('data/'+d+'.json', 'w') as result:
@@ -124,9 +124,12 @@ if __name__ == '__main__':
     # np.random.seed(1004)
     # random.seed(1004)
 
-    file_name = 'data/train_data.json'
-    tokenizer_path = 'data/tokenizer-wordlevel.json'
-    tokenizer = Tokenizer.from_file(tokenizer_path)
+    # file_name = 'data/train_data.json'
+    file_name = 'data/clean_anno.json'
+    split_train_test(file_name)
 
-    dataset = make_final_dataset(file_name, 2, tokenizer)
-    print(len(dataset[0]), dataset[0][0].shape, dataset[0][3].shape)
+    # tokenizer_path = 'data/tokenizer-wordlevel.json'
+    # tokenizer = Tokenizer.from_file(tokenizer_path)
+
+    # dataset = make_final_dataset(file_name, 2, tokenizer)
+    # print(len(dataset[0]), dataset[0][0].shape, dataset[0][3].shape)
