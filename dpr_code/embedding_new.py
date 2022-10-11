66c8b3251273d2b38cf9cbeec4632ce1eb6c3733
jiwoo <ann9902@gmail.com>
2022-10-11 16:54:18 +0900

    [tmp] tmp for sge com

--- dpr_code/embedding_new.py
+++ dpr_code/embedding_new.py
@@ -6,7 +6,8 @@ from tqdm import tqdm
 import json
 from dataset import make_tri_dataset_q
 from torch.utils.data import DataLoader, TensorDataset
-
+import numpy as np
+import sys
 
 def make_dataset(file_name, tokenizer):
     # 파일불러서 tri dataset 만들기
@@ -20,11 +21,18 @@ def make_dataset(file_name, tokenizer):
         for la in doc['latex']:
             latex.append(la)
             doc_id.append(doc['id'])
+    # torch.tensor(np.array(doc_id))
+    # print(f"latex : {sys.getsizeof(latex)}")
+    # print(f"doc_id : {sys.getsizeof(doc_id)}")
 
     tokenized_latex = tokenizer.encode_batch(latex)
+    # print(f"after tokenize : {sys.getsizeof(tokenized_latex)}")
+
     tri_dataset = make_tri_dataset_q(tokenized_latex)
+    # print(f"tri_dataset: {sys.getsizeof(tri_dataset)}")
     val_dataset = TensorDataset(
         tri_dataset['input_ids'], tri_dataset['token_type_ids'], tri_dataset['attention_mask'])
+    # print(f"final dataset : {sys.getsizeof(val_dataset)}")
     return val_dataset, doc_id
 
 
@@ -43,30 +51,38 @@ def embedding(file_name, tokenizer_path, model_path):
     encoder.eval()
 
     batch_size = 8
-    dataset, doc_id = make_dataset(file_name, tokenizer)
+    dataset, _ = make_dataset(file_name, tokenizer)
     dataloader = DataLoader(
             dataset, batch_size=batch_size, shuffle=False, drop_last=False)
-        
+    del dataset
     embs = []
     encoder.eval()
     with torch.no_grad():
         for i, batch in enumerate(tqdm(dataloader)):
-          batch = tuple(t.to(device) for t in batch)
+            batch = tuple(t.to(device) for t in batch)
 
-          inputs = {'input_ids': batch[0],
-                  'token_type_ids': batch[1],
-                  'attention_mask': batch[2]
-                  }
-          outputs = encoder(**inputs).pooler_output
-          embs.append(outputs.detach())
-          torch.cuda.empty_cache()
-          if i==2:
-            break
-    embs = torch.concat(embs, dim=0).tolist()
+            inputs = {'input_ids': batch[0],
+                    'token_type_ids': batch[1],
+                    'attention_mask': batch[2]
+                    }
+            outputs = encoder(**inputs).pooler_output.tolist()
+            with open('data/embeddings.txt', 'a') as f:
+                f.write(str(outputs)+'\n')
+            #   embs.append(outputs.detach())
+        
+            torch.cuda.empty_cache()
+            # if i==2:
+            #     break
+    # embs = torch.concat(embs, dim=0).tolist()
 
-    for em, do in zip(embs, doc_id):
-      with open('data/embeddings_76.txt', 'a') as f:
-        f.write(str(do)+' '+str(em)+'\n')
+    # for em, do in zip(embs, doc_id):
+    #     with open('data/embeddings_76.txt', 'a') as f:
+    #         f.write(str(do)+' '+str(em)+'\n')
     
 if __name__=="__main__":
   embedding("data/clean_anno.json", "data/tokenizer/tokenizer-bpe.json", 'model/ex/ep4_acc0.769')
+    
+    # tokenizer_path = "data/tokenizer/tokenizer-bpe.json"
+    # tokenizer = Tokenizer.from_file(tokenizer_path)
+    # dataset = make_dataset("data/clean_anno.json", tokenizer)
+    # print(dataset[0])
